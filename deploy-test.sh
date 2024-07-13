028e27b8b010cc4a30e1af02556fe8f238bb9e35
Santatra <razafindratsimba34@gmail.com>
2024-07-13 23:33:33 +0300

    add script for CD and test

---
+++ deploy-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+test_dest="/home/santatra/projet_kely/kotrana/resumed"
+theme="jsonresume-theme-onepage-plus"
+repo="git@github.com:HarimbolaSantatra/jsonresume-theme-onepage-plus.git"
+branch="test"
+
+cd $test_dest/node_modules
+if [ ! -e $theme ]; then
+    echo "Repository $theme not present!"
+    echo "Cloning ..."
+    git clone -b $branch $repo
+fi
+echo "Repository $theme is present!"
+cd $theme
+git pull origin $branch
+npm run make
